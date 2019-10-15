a10230fff7b30c1412758518c4957512e431acc0
weronikabar <56565114+weronikabar@users.noreply.github.com>
2019-10-15 19:47:59 +0200

    Update Main.java

--- i6e1-2s1/Main.java
+++ i6e1-2s1/Main.java
@@ -6,19 +6,19 @@ import com.joptimizer.optimizers.OptimizationRequest;
 
 public class Main {
     public static void main(String[] args) {
-        LinearMultivariateRealFunction objectiveFunction = new LinearMultivariateRealFunction(new double[]{-15,-34,-22},0);
+        LinearMultivariateRealFunction objectiveFunction = new LinearMultivariateRealFunction(new double[]{25,3},0);
 
         ConvexMultivariateRealFunction[] inequalities = new ConvexMultivariateRealFunction[5];
-        // p1 >= 0
-        inequalities[0] = new LinearMultivariateRealFunction(new double[]{-1.0, 0.00, 0.00}, 0.0);  // focus: -p1+0 <= 0
-        // p2 >= 0
-        inequalities[1] = new LinearMultivariateRealFunction(new double[]{0.0, -1.00, 0.00}, 0.0);  // focus: -p2+0 <= 0
-        // 6p1 + 3p2 >= 120
-        inequalities[2] = new LinearMultivariateRealFunction(new double[]{0.00, 0.00, -1.00}, 120.0); // focus: -6p1-3p2+120 <= 0
-        // p1 + 3p2 >= 60
-        inequalities[3] = new LinearMultivariateRealFunction(new double[]{5.0, 3.00, 0.00}, -3600.0);// focus: -p1-3p2+60 <= 0
-        // 9p1 + p2 >= 36
-        inequalities[4] = new LinearMultivariateRealFunction(new double[]{1.0,2.0,4.0},-4800);// focus: -9p1-p2+36 <= 0
+        // x1 >= 0
+        inequalities[0] = new LinearMultivariateRealFunction(new double[]{-1.0, 0.00}, 0.0);  // focus: -x1+0 <= 0
+        // x2 >= 0
+        inequalities[1] = new LinearMultivariateRealFunction(new double[]{0.0, -1.00}, 0.0);  // focus: -x2+0 <= 0
+        // x1*5+x2*2>=10
+        inequalities[2] = new LinearMultivariateRealFunction(new double[]{-5.00,-2.00}, 10.0); // focus: -5x1-2x2+10 <= 0
+        // x1*2+x2*9>=11
+        inequalities[3] = new LinearMultivariateRealFunction(new double[]{-2.0,-9.00}, 11.0);// focus: -2x1-9x2+11 <= 0
+        // x1*1+x2*3>=6
+        inequalities[4] = new LinearMultivariateRealFunction(new double[]{-1.0,-3.0},6);// focus: -x1-3x2+6 <= 0
 
         //optimization problem
         OptimizationRequest optimizationRequest = new OptimizationRequest();
@@ -38,10 +38,10 @@ public class Main {
         double[] solution = optimizer.getOptimizationResponse().getSolution();
         System.out.println("Length: " + solution.length);
         for (int i=0; i<solution.length/2; i++){
-            System.out.println( "X: " + Math.round(solution[i]) + "\tY: " + Math.round(solution[i+1])+ "\tZ: " + Math.round(solution[i+2]) );
+            System.out.println( "X1: " + Math.round(solution[i]) + "\tX2: " + Math.round(solution[i+1]) );
         }
 
-        System.out.println("A =" + (15d*Math.round(solution[0])+20d*Math.round(solution[1])+10d*Math.round(solution[2])));
+        System.out.println("Y =" + (25d*Math.round(solution[0])+3d*Math.round(solution[1])));
 
     }
 }
